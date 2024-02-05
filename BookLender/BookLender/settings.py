891651b6c5eec22ae084f01cc56abe32e66b7e1f
Rez27 <kadam09rohan@gmail.com>
2024-02-05 00:28:59 +0000

    Restructuring Files

--- BookLender/BookLender/settings.py
+++ BookLender/BookLender/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainapp',
 ]
 
 MIDDLEWARE = [
@@ -126,7 +127,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
