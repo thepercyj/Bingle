9859b216dd664a3faa4e5e5be29cc5941e144cb3
Rez27 <kadam09rohan@gmail.com>
2024-02-27 17:11:51 +0000

    Updated mainapp.urls

--- BookLender/BookLender/urls.py
+++ BookLender/BookLender/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mainapp.urls')),
-    path('', include('messagesApp.urls')),
-    path('', include('lendborrowapp.urls')),
+    path('main/', include('mainapp.urls')),
+    path('messages/', include('messagesApp.urls')),
+    path('lendborrowapp/', include('lendborrowapp.urls')),
 ]
