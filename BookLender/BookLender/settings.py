0b21f0d801996d02ac4d63d532eb7c1a2f796faa
Moka364mz <147619148+Moka364mz@users.noreply.github.com>
2024-03-06 19:51:12 +0000

    crispy added

--- BookLender/BookLender/settings.py
+++ BookLender/BookLender/settings.py
@@ -52,7 +52,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'mainapp',
     'messagesApp',
-    'lendborrowapp'
+    'lendborrowapp',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -88,6 +90,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'BookLender.wsgi.application'
 ASGI_APPLICATION = 'BookLender.asgi.application'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 
 # Database
@@ -156,3 +160,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
