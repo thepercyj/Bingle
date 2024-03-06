d91d4db44d17eb53e740151edd19ee68863b37be
Aman Thapa Magar (PJ) <aman.yhbs@gmail.com>
2024-03-06 19:38:04 +0000

    revert changes

--- BookLender/mainapp/views.py
+++ BookLender/mainapp/views.py
@@ -77,9 +77,64 @@ def profile(request):
     return render(request, 'profile_page.html', context)
 
 
+<<<<<<< HEAD
+=======
+def addBook(request):
+    """Processes the request to add a new book"""
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            new_book = form.save()
+            # Adds the book to the userBooks table
+            addUserBook(new_book)
+            return redirect('profile')
+        else:
+            # Form validation failed, return error details
+            return JsonResponse({'status': 'error', 'message': 'Form validation failed', 'errors': form.errors},
+                                status=400)
+    else:
+        # If the request method is not POST, inform the client appropriately
+        # Or render a form for GET requests if that's intended behavior
+        return HttpResponse('This endpoint expects a POST request.', status=405)
+
+    # As a last resort, return a generic response for unexpected cases
+    # This line should ideally never be reached if all cases are handled correctly above
+    return HttpResponse('Unexpected error occurred.', status=500)
+
+
+def addUserBook(request, book):
+    user_profile = UserProfile.objects.get(user=request.user)
+    """Adds a book to the user's library based on the Book Form submitted"""
+    new_user_book = UserBook(
+        owner_book_id=user_profile,  # Set the current user as the user_id
+        currently_with=user_profile,  # Defaults current user to currently_with on creation
+        book_id=book,  # Set the newly created book as the book_id
+        availability=True,  # Set available to true by default on creation
+        booked='No'
+    )
+
+    # Save the new userBooks instance to the database
+    new_user_book.save()
+
+
+@never_cache
+def removeBook(request):
+    user_profile = UserProfile.objects.get(user=request.user)
+    book_id = request.POST.get('book_id')
+    try:
+        book = UserBook.objects.get(id=book_id, owner_book_id=user_profile)
+        book.delete()
+        messages.success(request, "Book removed successfully.")
+    except UserBook.DoesNotExist:
+        messages.error(request, "Book not found.")
+    return HttpResponseRedirect(reverse('profile') + '?remove=true')
+
+
+>>>>>>> parent of c631ebc (Update views to match file structure)
 def updateProfile(request):
     if request.method == 'POST':
-        user = request.user
+        user_id = request.user
+        user = User.objects.get(id=user_id)
         user_profile = UserProfile.objects.get(user=user)  # Adjust based on your UserProfile model relation
 
         user.username = request.POST.get('inputUserName')
@@ -93,7 +148,7 @@ def updateProfile(request):
         user_profile.save()
 
         messages.success(request, "Profile updated successfully.")
-        return redirect('/main/profile_page')
+        return redirect('profile')
     else:
         # Handle non-POST request
         return render(request, 'profile_page.html')
