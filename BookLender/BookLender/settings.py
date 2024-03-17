9ba5d8dcc72e0f9b7a9e2d6aa7d5c6640063b2c5
Jawad-276267 <59816694+jawadnasar@users.noreply.github.com>
2024-03-17 21:12:21 +0000

    Fields Change
    Fields default values changed for better functionality
    returned_on -> has default value null
    review -> has a decimal value

--- BookLender/BookLender/settings.py
+++ BookLender/BookLender/settings.py
@@ -103,14 +103,25 @@ ASGI_APPLICATION = 'BookLender.asgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "esrs",
+#         "USER": "db",
+#         "PASSWORD": "esrsdb@2024##",
+#         "HOST": "144.21.56.167",
+#         "PORT": "6969",
+#     }
+# }
+
 DATABASES = {
     'default': {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "esrs",
-        "USER": "db",
-        "PASSWORD": "esrsdb@2024##",
-        "HOST": "144.21.56.167",
-        "PORT": "6969",
+        "USER": "root",
+        "PASSWORD": "root",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
