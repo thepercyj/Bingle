9e2e32402b26ec218d0b52211f1c31e54c5c436e
RobRyan1122 <114590371+RobRyan1122@users.noreply.github.com>
2024-04-16 15:49:36 +0000

    creating the recomendation app to be filled later

---
+++ BookLender/recomendationApp/recomendationalgo.py
@@ -0,0 +1,10 @@
+
+import random
+#just a tempory measuere while the book borrowing code is being worked on, this will be populated with the users recently borrowed genres
+borrowed_book= ["action","action","action","action","action","history","history","history","history","history","history"]
+
+# will be knn algo, right now just getting it to spit something out
+Recomended_Book = random.choice(borrowed_book)
+
+# just to test this is all running 
+print("recomended:", Recomended_Book)
