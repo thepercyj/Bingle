bc28896de6dd4e8c4718b236ef1a8dfc0982c2a9
RobRyan1122 <114590371+RobRyan1122@users.noreply.github.com>
2024-05-07 20:33:37 +0000

    now provides list of recomended titles based on users fave genre

--- BookLender/recommendations/views.py
+++ BookLender/recommendations/views.py
@@ -46,6 +46,34 @@ def getborrowed(request):
         # Append the genre of the book to the genres list
         genres.append(book.genre)
 
+    # create variable to store the most common genre
+    most_common_genre = None
+    # create variable to store the count of the most common genre
+    most_common_genre_count = 0
+    # iterate over the genres list
+    for genre in genres:
+        # count the number of times the genre appears in the list
+        genre_count = genres.count(genre)
+        # if the genre appears more times than the current most common genre
+        if genre_count > most_common_genre_count:
+            # update the most common genre and the count of the most common genre
+            most_common_genre = genre
+            most_common_genre_count = genre_count
+
+    # create a list to store the recommended books
+    recommended_books = []
+    # iterate over the books in the Book table
+    for book in Book.objects.all():
+        # if the book's genre is the most common genre
+
+        if len(recommended_books) == 5:
+            break
+        if book.genre == most_common_genre:
+            # add the book to the recommended books list
+            recommended_books.append(book)
+    
+    print(recommended_books)
+
     print(borrowed_books)
     print(genres)
     return JsonResponse({'borrowed_books': borrowed_books, 'our_profile_id': our_profile.id})
