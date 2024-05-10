444aed31eac42d56ad85048f4e6ed58f76ebd79e
Aman Thapa Magar (PJ) <aman.yhbs@gmail.com>
2024-05-10 16:59:55 +0100

    Renaming directories for docker compose

--- Bingle/Bingle/asgi.py
+++ Bingle/Bingle/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for BookLender project.
+ASGI config for Bingle project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BookLender.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Bingle.settings')
 
 application = get_asgi_application()
